f3066c8bb84395fb7914997ce363168c6e045ef0
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2023-07-31 11:16:10 +0700

    Mengubah ke dalam bentuk function

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // import scanner
         Scanner scanner = new Scanner(System.in);
-
-        // dataset karakter
         String[][] karakterData = {
                 {"Knight",
                         "Knight adalah pejuang yang ulung dengan kemampuan bertahan yang hebat. "
@@ -27,43 +24,58 @@ public class Main {
         System.out.println("Selamat datang di Pemilihan Karakter Fantasy!");
 
         // Menampilkan pilihan karakter kepada pengguna
+        displayKarakterOptions(karakterData);
+
+        // Meminta pengguna untuk memilih karakter
+        int choice = getValidChoice(scanner, karakterData.length);
+
+        String selectedCharacterName = karakterData[choice - 1][0];
+        String selectedCharacterDescription = karakterData[choice - 1][1];
+
+        // Menampilkan deskripsi karakter yang dipilih pengguna
+        displaySelectedCharacter(selectedCharacterName, selectedCharacterDescription);
+
+        System.out.println();
+        System.out.println("Selamat bermain permainan fantasy dengan karakter " + selectedCharacterName + " Anda!");
+        scanner.close();
+    }
+
+    // Fungsi untuk menampilkan pilihan karakter kepada pengguna
+    private static void displayKarakterOptions(String[][] karakterData) {
         System.out.println("Berikut adalah pilihan karakter yang tersedia:");
         System.out.println();
         for (int i = 0; i < karakterData.length; i++) {
             System.out.println((i + 1) + ". " + karakterData[i][0]);
         }
+    }
 
-        // Meminta pengguna untuk memilih karakter
-        int choice;
-        String selectedCharacterName = "";
-        String selectedCharacterDescription;
+    // Fungsi untuk meminta pengguna untuk memilih karakter dengan validasi input
+    private static int getValidChoice(Scanner scanner, int maxChoice) {
+        int choice = 0;
         boolean validChoice = false;
         while (!validChoice) {
             try {
                 System.out.println();
-                System.out.print("Masukkan nomor karakter yang Anda pilih (1-4): ");
+                System.out.print("Masukkan nomor karakter yang Anda pilih (1-" + maxChoice + "): ");
                 choice = scanner.nextInt();
 
-                if (choice >= 1 && choice <= karakterData.length) {
+                if (choice >= 1 && choice <= maxChoice) {
                     validChoice = true;
-                    // Mendapatkan deskripsi karakter yang dipilih pengguna
-                    selectedCharacterName = karakterData[choice - 1][0];
-                    selectedCharacterDescription = karakterData[choice - 1][1];
-
-                    // Menampilkan deskripsi karakter yang dipilih pengguna
-                    System.out.println();
-                    System.out.println("Anda memilih karakter " + selectedCharacterName + ".");
-                    System.out.println(selectedCharacterDescription);
                 } else {
                     System.out.println("Input tidak valid. Masukkan nomor karakter yang sesuai.");
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Input tidak valid. Masukkan nomor karakter yang sesuai.");
-                scanner.nextLine(); // digunakan untuk membersihkan buffer scanner dan maju ke baris berikutnya, sehingga program akan siap untuk menerima input yang baru.
+                scanner.nextLine(); // membersihkan buffer scanner dan maju ke baris berikutnya
             }
         }
+        return choice;
+    }
+
+    // Fungsi untuk menampilkan deskripsi karakter yang dipilih pengguna
+    private static void displaySelectedCharacter(String selectedCharacterName, String selectedCharacterDescription) {
         System.out.println();
-        System.out.println("Selamat bermain permainan fantasy dengan karakter " + selectedCharacterName + " Anda!");
-        scanner.close();
+        System.out.println("Anda memilih karakter " + selectedCharacterName + ".");
+        System.out.println(selectedCharacterDescription);
     }
 }
